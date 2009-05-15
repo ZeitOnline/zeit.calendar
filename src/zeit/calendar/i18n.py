21a535f5085e1238c03de48e94457f31dacc7e3c
Christian Zagrodnick <cz@gocept.com>
2009-05-15 12:46:25 +0000

    remove localsation and use cms domain

--- src/zeit/calendar/i18n.py
+++ src/zeit/calendar/i18n.py
@@ -1,7 +1,4 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import zope.i18nmessageid
-
-
-MessageFactory = zope.i18nmessageid.MessageFactory('zeit.calendar')
+from zeit.cms.i18n import MessageFactory

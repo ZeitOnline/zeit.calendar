592fa9214283fb90b8755fc8cb15a6260a793832
Christian Zagrodnick <cz@gocept.com>
2010-04-27 07:50:26 +0000

    Preparing release 1.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.calendar',
-    version = '1.6.2dev',
+    version = '1.6.2',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.calendar',

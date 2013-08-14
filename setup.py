fa95d0871e9f5e55eae0a12efe359a9fd8f5388f
Wolfgang Schnerring <ws@gocept.com>
2013-08-14 13:13:08 +0200

    Back to development: 1.6.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.calendar',
-    version='1.6.7',
+    version='1.6.8.dev0',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://svn.gocept.com/repos/gocept-int/zeit.cms/zeit.calendar',
